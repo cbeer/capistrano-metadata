bcd6e9f55c0646c9c5fa1a565cfb18f895b7b93a
Chris Beer <cabeer@stanford.edu>
2018-02-06 07:41:35 -0800

    Initial commit

---
+++ lib/capistrano/metadata.rb
@@ -0,0 +1,9 @@
+require "capistrano/metadata/version"
+
+module Capistrano
+  module Metadata
+    # Your code goes here...
+  end
+end
+
+load File.expand_path('../tasks/deploy_metadata.cap', __FILE__)
